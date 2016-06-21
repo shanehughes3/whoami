56d8e03a7c9b5345c7c5c795d21aae5ad8895f9f
Shane Hughes <shanehughes314@gmail.com>
2016-06-21 15:33:53 -0500

    fix ip prefix

--- whoami.js
+++ whoami.js
@@ -2,10 +2,11 @@ var express = require("express"),
     app = express();
 
 app.get("/", function(req, res) {
+    var ip = req.connection.remoteAddress.split(':').pop();
     var lang = req.headers["accept-language"].split(',');
     var os = /\(([^)]+)\)/.exec(req.headers["user-agent"]);
     var output = {
-	ip: req.connection.remoteAddress,
+	ip: ip,
 	language: lang[0],
 	os: os[1]
     };
